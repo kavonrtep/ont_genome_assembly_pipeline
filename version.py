0671394868b799bef938ffd7626caf3f01b99f10
Petr Novak <petr@umbr.cas.cz>
2026-07-23 16:31:38 +0200

    bump version to 0.2.3

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Single source of truth for the genome assembly pipeline version."""
 
-__version__ = "0.2.2"
-__version_info__ = (0, 2, 2, None)
+__version__ = "0.2.3"
+__version_info__ = (0, 2, 3, None)
 
 
 def parse_version(s):
